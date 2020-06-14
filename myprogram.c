eff8b00d3ec48615cd9afb90b4b9f423df949293
monaliPalhal <63635626+monaliPalhal@users.noreply.github.com>
2020-06-14 17:00:03 +0530

    Create myprogram.c

---
+++ myprogram.c
@@ -0,0 +1,9 @@
+#include ”types.h”
+#include ”stat.h”
+#include ”user.h”
+#include ”fcntl.h”
+int main(int argc,char *argv[5]){
+	printf(1,"call fuction to match given string with read sring from file",Lseek1(argv[1],argv[2],argv[3],argv[4]);
+	printf(1,"call function to match given file with new copied file",Lseek2(argv[1]);
+exit();	
+}
