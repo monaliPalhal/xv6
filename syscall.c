e0c88b21b6c822de96b503aa99ac6591a299059d
monaliPalhal <63635626+monaliPalhal@users.noreply.github.com>
2020-06-14 16:47:07 +0530

    Update syscall.c

--- syscall.c
+++ syscall.c
@@ -103,8 +103,8 @@ extern int sys_unlink(void);
 extern int sys_wait(void);
 extern int sys_write(void);
 extern int sys_uptime(void);
-extern void sys_Lseek1(char *name,int offset,int len,char *string);
-extern void sys_Lseek2(char *name);
+extern void sys_Lseek1(char* ,int, int, char*);
+extern void sys_Lseek2(char*);
 
 static int (*syscalls[])(void) = {
 [SYS_fork]    sys_fork,
